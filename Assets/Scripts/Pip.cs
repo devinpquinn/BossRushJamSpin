c14de19876778c3b8afaf392559d45b533c4abe0
Devin <devinpquinn4@gmail.com>
2025-01-18 15:49:53 -0500

    pip work

---
+++ Assets/Scripts/Pip.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pip : MonoBehaviour
+{
+	private Image image;
+	private RectTransform rectTransform;
+	public Color markedColor;
+	
+	void Awake()
+	{
+		image = GetComponent<Image>();
+		rectTransform = GetComponent<RectTransform>();
+	}
+	
+	public void Mark()
+	{
+		StartCoroutine(Flash());
+	}
+	
+	private System.Collections.IEnumerator Flash()
+	{
+		//set color to markedColor and scale local scale from 2 to 1 over 0.5 seconds
+		image.color = markedColor;
+		rectTransform.localScale = Vector3.one * 2;
+		
+		float t = 0;
+		while (t < 0.5f)
+		{
+			t += Time.deltaTime;
+			rectTransform.localScale = Vector3.one * Mathf.Lerp(2, 1, t / 0.5f);
+			yield return null;
+		}
+	}
+}
--- Assets/Scripts/ProgressPips.cs
+++ Assets/Scripts/ProgressPips.cs
@@ -4,24 +4,23 @@ using UnityEngine.UI;
 
 public class ProgressPips : MonoBehaviour
 {
-	private List<GameObject> pips;
+	private List<Pip> pips;
 	public GameObject pipPrefab;
-	public Color markedColor;
 	
 	public void SetupPips(int numPips)
 	{
 		//spawn numPips pips as children of this object
-		pips = new List<GameObject>();
+		pips = new List<Pip>();
 		for (int i = 0; i < numPips; i++)
 		{
 			GameObject pip = Instantiate(pipPrefab, transform);
-			pips.Add(pip);
+			pips.Add(pip.gameObject.GetComponent<Pip>());
 		}
 	}
 	
 	public void MarkPip(int pipIndex)
 	{
 		//change color of pip image at pipIndex to markedColor
-		pips[pipIndex].GetComponent<Image>().color = markedColor;
+		pips[pipIndex].Mark();
 	}
 }
