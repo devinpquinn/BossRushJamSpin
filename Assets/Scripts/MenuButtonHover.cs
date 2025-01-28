a7d0fd69697211b54e28cb5bc2d7603b1b2cf85b
Devin <devinpquinn4@gmail.com>
2025-01-27 21:45:09 -0500

    interim

--- Assets/Scripts/MenuButtonHover.cs
+++ Assets/Scripts/MenuButtonHover.cs
@@ -1,26 +1,63 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections;
 
 public class MenuButtonHover : MonoBehaviour
 {
 	public TextMeshProUGUI text;
+	
+	private RectTransform rectTransform;
+	private Coroutine hoverCoroutine;
 	
 	private Color baseColor;
 	private Color hoverColor = Color.white;
 	
 	void Start()
 	{
+		rectTransform = GetComponent<RectTransform>();
 		baseColor = text.color;
 	}
 	
 	public void OnHover()
 	{
-		text.color = hoverColor;
+		if(hoverCoroutine != null)
+			StopCoroutine(hoverCoroutine);
+		hoverCoroutine = StartCoroutine(Hover());
 	}
 	
 	public void OnExit()
 	{
+		if(hoverCoroutine != null)
+			StopCoroutine(hoverCoroutine);
+		hoverCoroutine = StartCoroutine(Exit());
+	}
+	
+	IEnumerator Hover()
+	{
+		text.color = hoverColor;
+		
+		//lerp scale to 1.1 over 0.1 seconds
+		float t = 0;
+		while (t < 0.1f)
+		{
+			t += Time.deltaTime;
+			rectTransform.localScale = Vector3.Lerp(Vector3.one, Vector3.one * 1.1f, t / 0.1f);
+			yield return null;
+		}
+	}
+	
+	IEnumerator Exit()
+	{
 		text.color = baseColor;
+		
+		//lerp scale to 1 over 0.1 seconds
+		float t = 0;
+		while (t < 0.1f)
+		{
+			t += Time.deltaTime;
+			rectTransform.localScale = Vector3.Lerp(Vector3.one * 1.1f, Vector3.one, t / 0.1f);
+			yield return null;
+		}
 	}
 }
